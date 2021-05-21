60df36d690bea240a5a829baf4b982bed3197f2f
wykolor <kolor1121460147@163.com>
2021-05-21 18:11:49 +0800

    feat: 添加查询组件-radio，checkbox，input等组件的支持

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <!-- 查询组件 -->
-    <KolorQueryForm :formItems="formItems" v-model="model" @form-item-change="formItemChange"  />
+    <KolorQueryForm :formItems="formItems" v-model="model" @form-item-change="formItemChange" />
     <!-- 表单组件 -->
     <KolorTable 
       :data="tableData" 
@@ -155,7 +155,7 @@ export default {
         },
         {
           type: 'select',
-          prop: '',
+          prop: null,
           label: '班型',
           itemLists: [
             {
@@ -194,7 +194,7 @@ export default {
         },
         {
           type: 'select',
-          prop: '',
+          prop: null,
           label: '年份',
           itemLists: [
             {
@@ -215,127 +215,301 @@ export default {
               ]
             }
           ],
+        },
+        {
+          type: 'input',
+          prop: 'studentNumber',
+          label: '学生ＩＤ',
+          // isAutoComplete: false, // 新增
+          itemList: {
+            type: 'text',
+            clearable: true
+          },
+        },
+        {
+          type: 'timePicker',
+          prop: null,
+          label: '学生上学时间',
+          itemLists: [
+            {
+              prop: 'startTime',
+              timePickerSelect: true, // 是否使用时间下拉选择
+              'picker-options': {
+                start: '08:30',
+                step: '00:15',
+                end: '18:30'
+              }
+            },
+            {
+              prop: 'endTime',
+              'is-range': true,
+            }
+          ]
+        },
+        {
+          type: 'datePicker',
+          prop: null,
+          label: '学生报名日期',
+          itemLists: [
+            {
+              prop: 'startDate',
+              type: 'date',
+            },
+            {
+              prop: 'endDate',
+              type: 'datetime'
+            }
+          ]
+        },
+        {
+          type: 'switch',
+          prop: 'isSchool',
+          label: '是否在上学',
+          itemList: {
+            'active-color': "red"
+          }
+        },
+        {
+          type: 'cascader',
+          prop: 'address',
+          label: '地址',
+          isCascaderPanel: false, // 是否为级联面板
+          itemList: {
+             options: [{
+          value: 'zhinan',
+          label: '指南',
+          children: [{
+            value: 'shejiyuanze',
+            label: '设计原则',
+            children: [{
+              value: 'yizhi',
+              label: '一致'
+            }, {
+              value: 'fankui',
+              label: '反馈'
+            }, {
+              value: 'xiaolv',
+              label: '效率'
+            }, {
+              value: 'kekong',
+              label: '可控'
+            }]
+          }, {
+            value: 'daohang',
+            label: '导航',
+            children: [{
+              value: 'cexiangdaohang',
+              label: '侧向导航'
+            }, {
+              value: 'dingbudaohang',
+              label: '顶部导航'
+            }]
+          }]
+        }, {
+          value: 'zujian',
+          label: '组件',
+          children: [{
+            value: 'basic',
+            label: 'Basic',
+            children: [{
+              value: 'layout',
+              label: 'Layout 布局'
+            }, {
+              value: 'color',
+              label: 'Color 色彩'
+            }, {
+              value: 'typography',
+              label: 'Typography 字体'
+            }, {
+              value: 'icon',
+              label: 'Icon 图标'
+            }, {
+              value: 'button',
+              label: 'Button 按钮'
+            }]
+          }, {
+            value: 'form',
+            label: 'Form',
+            children: [{
+              value: 'radio',
+              label: 'Radio 单选框'
+            }, {
+              value: 'checkbox',
+              label: 'Checkbox 多选框'
+            }, {
+              value: 'input',
+              label: 'Input 输入框'
+            }, {
+              value: 'input-number',
+              label: 'InputNumber 计数器'
+            }, {
+              value: 'select',
+              label: 'Select 选择器'
+            }, {
+              value: 'cascader',
+              label: 'Cascader 级联选择器'
+            }, {
+              value: 'switch',
+              label: 'Switch 开关'
+            }, {
+              value: 'slider',
+              label: 'Slider 滑块'
+            }, {
+              value: 'time-picker',
+              label: 'TimePicker 时间选择器'
+            }, {
+              value: 'date-picker',
+              label: 'DatePicker 日期选择器'
+            }, {
+              value: 'datetime-picker',
+              label: 'DateTimePicker 日期时间选择器'
+            }, {
+              value: 'upload',
+              label: 'Upload 上传'
+            }, {
+              value: 'rate',
+              label: 'Rate 评分'
+            }, {
+              value: 'form',
+              label: 'Form 表单'
+            }]
+          }, {
+            value: 'data',
+            label: 'Data',
+            children: [{
+              value: 'table',
+              label: 'Table 表格'
+            }, {
+              value: 'tag',
+              label: 'Tag 标签'
+            }, {
+              value: 'progress',
+              label: 'Progress 进度条'
+            }, {
+              value: 'tree',
+              label: 'Tree 树形控件'
+            }, {
+              value: 'pagination',
+              label: 'Pagination 分页'
+            }, {
+              value: 'badge',
+              label: 'Badge 标记'
+            }]
+          }, {
+            value: 'notice',
+            label: 'Notice',
+            children: [{
+              value: 'alert',
+              label: 'Alert 警告'
+            }, {
+              value: 'loading',
+              label: 'Loading 加载'
+            }, {
+              value: 'message',
+              label: 'Message 消息提示'
+            }, {
+              value: 'message-box',
+              label: 'MessageBox 弹框'
+            }, {
+              value: 'notification',
+              label: 'Notification 通知'
+            }]
+          }, {
+            value: 'navigation',
+            label: 'Navigation',
+            children: [{
+              value: 'menu',
+              label: 'NavMenu 导航菜单'
+            }, {
+              value: 'tabs',
+              label: 'Tabs 标签页'
+            }, {
+              value: 'breadcrumb',
+              label: 'Breadcrumb 面包屑'
+            }, {
+              value: 'dropdown',
+              label: 'Dropdown 下拉菜单'
+            }, {
+              value: 'steps',
+              label: 'Steps 步骤条'
+            }]
+          }, {
+            value: 'others',
+            label: 'Others',
+            children: [{
+              value: 'dialog',
+              label: 'Dialog 对话框'
+            }, {
+              value: 'tooltip',
+              label: 'Tooltip 文字提示'
+            }, {
+              value: 'popover',
+              label: 'Popover 弹出框'
+            }, {
+              value: 'card',
+              label: 'Card 卡片'
+            }, {
+              value: 'carousel',
+              label: 'Carousel 走马灯'
+            }, {
+              value: 'collapse',
+              label: 'Collapse 折叠面板'
+            }]
+          }]
+        }, {
+          value: 'ziyuan',
+          label: '资源',
+          children: [{
+            value: 'axure',
+            label: 'Axure Components'
+          }, {
+            value: 'sketch',
+            label: 'Sketch Templates'
+          }, {
+            value: 'jiaohu',
+            label: '组件交互文档'
+          }]
+        }]
+          }
+        },
+        {
+          type: 'radio',
+          prop: 'hobby',
+          label: '爱好',
+          groupType: '', // 单选框格式 可选值 normal| button
+          groupProps: {}, // Radio-group Attributes
+          itemLists: [
+            // Radio Attributes
+            {
+              label: 'sing',
+            },
+            {
+              label: 'jump',
+            }
+          ]
+        },
+        {
+          type: 'checkbox',
+          prop: 'eatList',
+          label: '今晚吃啥',
+          groupType: 'button', // 复选框样式 可选值 normal | button,
+          groupProps: {}, // checkbox-Group Attributes
+          itemLists: [
+            {
+              label: '大盘鸡'
+            },
+            { 
+              label: '西北面'
+            }
+          ]
         }
       ],
       model: {
-        region: 'kolor'
+        eatList: []
       }
     }
   },
-  created() {
-    // setTimeout(() => {
-    //   this.tableData = [
-    //     {
-    //       id: 1,
-    //       name: 'kolor',
-    //       age: 18,
-    //       sex: 'female'
-    //     },
-    //     {
-    //       id: 2,
-    //       name: '王嘉尔',
-    //       age: 19,
-    //       sex: 'male'
-    //     },
-    //     {
-    //       id: 1,
-    //       name: 'kolor',
-    //       age: 18,
-    //       sex: 'female'
-    //     },
-    //     {
-    //       id: 2,
-    //       name: '王嘉尔',
-    //       age: 19,
-    //       sex: 'male'
-    //     },
-    //     {
-    //       id: 1,
-    //       name: 'kolor',
-    //       age: 18,
-    //       sex: 'female'
-    //     },
-    //     {
-    //       id: 2,
-    //       name: '王嘉尔',
-    //       age: 19,
-    //       sex: 'male'
-    //     },
-    //     {
-    //       id: 1,
-    //       name: 'kolor',
-    //       age: 18,
-    //       sex: 'female'
-    //     },
-    //     {
-    //       id: 2,
-    //       name: '王嘉尔',
-    //       age: 19,
-    //       sex: 'male'
-    //     },
-    //     {
-    //       id: 1,
-    //       name: 'kolor',
-    //       age: 18,
-    //       sex: 'female'
-    //     },
-    //     {
-    //       id: 2,
-    //       name: '王嘉尔',
-    //       age: 19,
-    //       sex: 'male'
-    //     },
-    //     {
-    //       id: 1,
-    //       name: 'kolor',
-    //       age: 18,
-    //       sex: 'female'
-    //     },
-    //     {
-    //       id: 2,
-    //       name: '王嘉尔',
-    //       age: 19,
-    //       sex: 'male'
-    //     },
-    //     {
-    //       id: 1,
-    //       name: 'kolor',
-    //       age: 18,
-    //       sex: 'female'
-    //     },
-    //     {
-    //       id: 2,
-    //       name: '王嘉尔',
-    //       age: 19,
-    //       sex: 'male'
-    //     },
-    //     {
-    //       id: 1,
-    //       name: 'kolor',
-    //       age: 18,
-    //       sex: 'female'
-    //     },
-    //     {
-    //       id: 2,
-    //       name: '王嘉尔',
-    //       age: 19,
-    //       sex: 'male'
-    //     },
-    //     {
-    //       id: 1,
-    //       name: 'kolor',
-    //       age: 18,
-    //       sex: 'female'
-    //     },
-    //     {
-    //       id: 2,
-    //       name: '王嘉尔',
-    //       age: 19,
-    //       sex: 'male'
-    //     },
-    //   ]
-    // }, 5000)
-  },
+  created() {},
   components: {
     KolorTable,
     KolorQueryForm
@@ -351,7 +525,8 @@ export default {
       this.$refs.multipleTable.ElTable().clearSelection();
     },
     formItemChange(value) {
-      console.log('表单湘发生变化', value)
+      console.log('表单湘发生变化', value);
+      console.log(this.model);
     }
   }
 }
--- src/components/kolor-query-form-jsx.vue
+++ src/components/kolor-query-form-jsx.vue
@@ -17,22 +17,91 @@ export default {
     }
   },
   methods: {
+    /**格式化表单控件 */ 
     formatFormItem (formItem) {
       let itemEl = null;
       switch (formItem.type) {
         case 'select': 
-          itemEl = formItem.itemLists.map(item => (
-            <el-select props={item} v-model={this.model[item.prop]} on={{change: (value) => this.$emit('form-item-change', {prop: item.prop, value})}}>
+          itemEl = formItem.itemLists && formItem.itemLists.map(item => (
+            <el-select props={item} v-model={this.model[item.prop]} on={{...this.formatFormItemEvents(formItem, item.prop)}}>
               {item.dataLists.map(list => (
                 <el-option props={list} label={list[item.formatDataKey && item.formatDataKey.label ? item.formatDataKey.label : 'label']} value={list[item.formatDataKey && item.formatDataKey.value ? item.formatDataKey.value : 'value']} key={list.value}></el-option>
               ))}
             </el-select>
+          ));
+          break;
+        case 'input': 
+          itemEl = (formItem.isAutoComplete ?
+            <el-autocomplete props={formItem.itemList} v-model={this.model[formItem.prop]} on={{...this.formatFormItemEvents(formItem)}}></el-autocomplete>
+           : 
+            <el-input props={formItem.itemList} v-model={this.model[formItem.prop]} on={{...this.formatFormItemEvents(formItem)}}></el-input>
+          ) 
+          break;
+        case 'timePicker': 
+          itemEl = formItem.itemLists && formItem.itemLists.map(item => (
+            item.timePickerSelect ?
+            <el-time-select props={item} v-model={this.model[item.prop]} on={{...this.formatFormItemEvents(formItem, item.prop)}}></el-time-select>
+            :
+            <el-time-picker props={item} v-model={this.model[item.prop]} on={{...this.formatFormItemEvents(formItem, item.prop)}}></el-time-picker>
           ))
+          break;
+        case 'datePicker': 
+          itemEl = formItem.itemLists && formItem.itemLists.map(item => (
+            <el-date-picker props={item} v-model={this.model[item.prop]} on={{...this.formatFormItemEvents(formItem, item.prop)}}></el-date-picker>
+          ))
+          break;
+        case 'switch': 
+          itemEl = <el-switch props={formItem.itemList} v-model={this.model[formItem.prop]} on={{...this.formatFormItemEvents(formItem)}}></el-switch>
+          break;
+        case 'cascader': 
+          itemEl = formItem.isCascaderPanel ? 
+          <el-cascader-panel props={formItem.itemList} v-model={this.model[formItem.prop]} on={{...this.formatFormItemEvents(formItem)}}></el-cascader-panel>
+          :
+          <el-cascader props={formItem.itemList} v-model={this.model[formItem.prop]} on={{...this.formatFormItemEvents(formItem)}}></el-cascader>
+          break;
+        case 'radio': 
+          itemEl = <el-radio-group props={formItem.groupProps} v-model={this.model[formItem.prop]} on={{...this.formatFormItemEvents(formItem)}}>
+            {formItem.itemLists && formItem.itemLists.map(item => (
+              formItem.groupType === 'button' ?
+                <el-radio-button props={item}></el-radio-button>
+                :
+                <el-radio props={item}></el-radio>
+            ))}
+          </el-radio-group>
+          break;
+        case 'checkbox': 
+          itemEl = <el-checkbox-group props={formItem.groupProps} v-model={this.model[formItem.prop]} on={{...this.formatFormItemEvents(formItem)}}>
+            {formItem.itemLists && formItem.itemLists.map(item => (
+              formItem.groupType === 'button' ?
+                <el-checkbox-button props={item}></el-checkbox-button>
+                :
+                <el-checkbox props={item}></el-checkbox>
+            ))}
+          </el-checkbox-group>
           break;
         case 'default': 
           itemEl = null;
       }
       return itemEl;
+    },
+
+    /**格式化表单控件的事件处理, 暴露给父组件监听 */
+    formatFormItemEvents(formItem, prop = formItem.prop) {
+      const on = {
+        change: value => this.$emit('form-item-change', { prop, value })
+      };
+      // switch (formItem.type) {
+      //   case 'select': 
+      //     on['change'] = value => this.$emit('form-item-change', { prop, value });
+      //     break;
+      //   case 'input': 
+      //     on['change'] = value => this.$emit('form-item-change', { prop, value });
+      //     break;
+          
+      //   case 'default': 
+      //     break;
+      // }
+      return on;
     }
   },
   render() {
